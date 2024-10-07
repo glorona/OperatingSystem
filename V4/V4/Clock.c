e43f6e2181848b9d3155e8b1381ec97495f979b9
Gabriel Lorona <glorona@espol.edu.ec>
2024-10-07 12:11:56 -0500

    Agregar v4

---
+++ V4/V4/Clock.c
@@ -0,0 +1,20 @@
+// V1
+#include "Clock.h"
+#include "Processor.h"
+#include "ComputerSystemBase.h"
+
+int tics=0;
+
+
+void Clock_Update() {
+	tics++;
+	if(tics%intervalBetweenInterrupts==0){
+		Processor_RaiseInterrupt(CLOCKINT_BIT);
+	}
+}
+
+
+int Clock_GetTime() {
+
+	return tics;
+}
---
+++ V4/V4/Processor.h
@@ -0,0 +1,42 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+#include "MainMemory.h"
+#include "ProcessorBase.h"
+
+#define INTERRUPTTYPES 10
+#define CPU_SUCCESS 1
+#define CPU_FAIL 0
+
+// Enumerated type that connects bit positions in the PSW register with
+// processor events and status
+enum PSW_BITS {POWEROFF_BIT=0, ZERO_BIT=1, NEGATIVE_BIT=2, OVERFLOW_BIT=3, EXECUTION_MODE_BIT=7, INTERRUPT_MASKED_BIT=15};
+
+// Enumerated type that connects bit positions in the interruptLines with
+// interrupt types 
+enum INT_BITS {SYSCALL_BIT=2, EXCEPTION_BIT=6, CLOCKINT_BIT=9};
+
+//V4
+enum EXCEPTIONS {DIVISIONBYZERO, INVALIDPROCESSORMODE, INVALIDADDRESS, INVALIDINSTRUCTION};
+
+// Functions prototypes
+void Processor_InitializeInterruptVectorTable(int);
+int Processor_FetchInstruction();
+void Processor_InstructionCycleLoop();
+void Processor_DecodeAndExecuteInstruction();
+void Processor_RaiseInterrupt(const unsigned int);
+void Processor_ManageInterrupts();
+
+//The OS needs to access registerC
+void Processor_SetRegisterC(int);
+
+char * Processor_ShowPSW();
+int Processor_GetCTRL();
+void Processor_SetCTRL(int);
+
+//Get y Set para registro D
+int Processor_GetRegisterD();
+void Processor_SetRegisterD(int);
+
+
+#endif
